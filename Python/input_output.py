fc1cf66611cf5587f05040077f844d8fba736e96
Kai Reinhold <keena.reinhold07@gmail.com>
2024-08-30 08:56:51 -0700

    exported from Skill Struck

---
+++ Python/input_output.py
@@ -0,0 +1,4 @@
+#Whatever you want to get as input must have something to prompt the user.
+name = input("What is your name? ")
+#to print inputs with other pieces, you must put spaces and make sure that each piece that is being printed either has quotes, or is the name of a variable that recieved an input from the user previously. Make sure to add + between pieces that are being concatenated.
+print("Hello, " + name + "!")
