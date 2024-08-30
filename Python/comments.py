b0eb98c943325afdac7c8b73377ec7bddb17233d
Kai Reinhold <keena.reinhold07@gmail.com>
2024-08-30 09:07:19 -0700

    exported from Skill Struck

--- Python/comments.py
+++ Python/comments.py
@@ -1,3 +1,4 @@
 #Comments must be preceeded by a hash symbol. If not, they will be run as code. ex:
 print("This line will print!")
 #print("And this line will not, even though it is proper code.")
+#This is a test
