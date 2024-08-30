38d4fba2ad7e69015d575c878eff3f84014d8c44
Kai Reinhold <keena.reinhold07@gmail.com>
2024-08-30 08:56:48 -0700

    exported from Skill Struck

---
+++ Python/print.py
@@ -0,0 +1,2 @@
+#Print statements can be typed with ' or ", but they have to be the same throughout the line. If you want to print "World's" or another word with an apostrophe, put " around the statement that you want to print.
+print("Hello World!");
