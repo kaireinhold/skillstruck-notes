de3bfa41830d491564f506a4550f9356b233a3af
Kai Reinhold <keena.reinhold07@gmail.com>
2024-12-02 10:05:22 -0700

    exported from Skill Struck

---
+++ JavaScript/intro.js
@@ -0,0 +1,10 @@
+// How to declare a variable:
+var myname = "Kai";
+// How to output to console:
+console.log(myname);
+// Data types:
+var is_string = "string";
+var is_integer = 10;
+var is_boolean = true; //or false
+var is_null = null; //null essentially means that the variable is empty.
+console.log(is_string, is_integer, is_boolean, is_null) //if you run this, the console will output 'string 10 true'. See how the null variable isn't technically shown? That's because it's empty.
