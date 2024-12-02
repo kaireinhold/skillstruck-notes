f24a9871a381cba6b6abbb00c4f3020abc41f717
Kai Reinhold <keena.reinhold07@gmail.com>
2024-12-02 10:05:28 -0700

    exported from Skill Struck

---
+++ JavaScript/arrays.js
@@ -0,0 +1,10 @@
+//Arrays can hold multiple values, and even values of different data types can be in the same array.
+var players = ["Grian", "Scar", "Pearl", "Gem"];
+console.log(players);
+var scores = [33, 37, 43, 28];
+console.log(scores);
+var mixed = ["Grian",33,"Scar",37,"Pearl",43,"Gem",28];
+console.log(mixed);
+
+//Accessing elements in an Array
+//Indexing
