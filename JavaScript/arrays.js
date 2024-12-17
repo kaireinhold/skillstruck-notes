0d215ae0869554f1752fb62db6c5ecedc4abf257
Kai Reinhold <keena.reinhold07@gmail.com>
2024-12-17 09:55:14 -0700

    exported from Skill Struck

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -35,10 +35,23 @@ winners.push("Grian", "Scott", "Pearl", "Martyn", "Scar", "Cleo");
 console.log(winners); //Now outputs the array ["Grian","Scott","Pearl","Martyn","Scar","Cleo"]
 
 //Removing from Arrays
-mixed.pop();
-var listener = mixed.pop();
+//mixed.pop();
+//var listener = mixed.pop();
 //Pop can only remove the last item in the array, no matter what is put in the parentheses (think function with no parameters)
 
 console.log(mixed);
-console.log(listener);
+//console.log(listener);
 
+//Inserting into Arrays
+winners.splice(6, 0, 'Joel');
+console.log(winners);
+
+//Changing elements in Arrays
+var names = ["Nicole", "Justin", "Brian"];
+names[1] = "Jill";
+console.log(names);
+
+//Converting Arrays to Strings
+var stringArray1 = mixed.toString();
+console.log(stringArray1);
+console.log("The players and their scores are: " + stringArray1);
